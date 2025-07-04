5b7d49aa5fe02759280b8681979ba36e5c8baf5a
Abd. Asis <id.abdasis@gmail.com>
2025-07-04 13:12:52 +0700

    setup shadcn

--- inertia/pages/suppliers/index.tsx
+++ inertia/pages/suppliers/index.tsx
@@ -1,18 +1,17 @@
-import { Link } from '@inertiajs/react';
-import { PageTitle } from '~/components/page-title';
-import { Button } from '~/components/ui/button';
-import AppLayout from '~/layouts/app-layout';
+import { PageTitle } from '@/components/page-title'
+import { Button } from '@/components/ui/button'
+import AppLayout from '@/layouts/app-layout'
+import { Link } from '@inertiajs/react'
 const Suppliers = () => {
-    return (
-        <AppLayout>
-            <div className='flex justify-between items-center'>
-                <PageTitle title='Suppliers' subtitle='Data untuk mengatur suppliers' />
-                <Link
-                    href="/suppliers/create">
-                    <Button>Tambah Supplier</Button>
-                </Link>
-            </div>
-        </AppLayout>
-    )
+  return (
+    <AppLayout>
+      <div className="flex justify-between items-center">
+        <PageTitle title="Suppliers" subtitle="Data untuk mengatur suppliers" />
+        <Link href="/suppliers/create">
+          <Button>Tambah Supplier</Button>
+        </Link>
+      </div>
+    </AppLayout>
+  )
 }
-export default Suppliers;
+export default Suppliers
